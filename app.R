1a4725d33255abc5c63310ad9011c94d888beabb
Heili Lowman <heilil@sccwrp.org>
2020-09-25 10:26:59 -0700

    Added pipes for data tidying prior to use in figures on tab panel #4.

--- app.R
+++ app.R
@@ -4,6 +4,8 @@
 
 #### Setup ####
 
+# Anything that should only happen ONCE should be placed in the setup section, prior to the actual shiny structure.
+
 # Load packages
 library(tidyverse)
 library(patchwork)
@@ -11,10 +13,47 @@ library(calecopal)
 library(shiny)
 library(shinythemes)
 
-# Load finalized dataset
+# Load finalized dataset.
 aoc <- read_csv("AquaticOrganisms_Clean_final.csv", guess_max = 10000)
 
-# Create Shiny app
+# Add log transformed concentration columns for easier plotting below.
+aoc$log_dose.mg.L <- log10(aoc$dose.mg.L)
+aoc$log_dose.particles.mL <- log10(aoc$dose.particles.mL)
+
+# Add factor and releved effects column.
+aoc$effect_f <- factor(aoc$effect, levels = c("Y", "N"))
+
+#### Leah Setup ####
+
+#### Emily Setup ####
+
+#### Heili Setup ####
+
+# Master dataset for scatterplots - for Heili's tab.
+aoc_y <- aoc %>% # start with original dataset
+  # full dataset filters.
+  filter(effect == "Y") %>% # only includes those datapoints with demonstrated effects.
+  filter(environment != "Terrestrial") %>% # removes terrestrial data.
+  # size category data tidying.
+  mutate(size.category.noNA = replace_na(size.category, 0)) %>% # replaces NA with 0 so we can better relabel it.
+  mutate(size_cat = case_when(
+    size.category.noNA == 1 ~ "<1µ",
+    size.category.noNA == 2 ~ "1µ < 10µ",
+    size.category.noNA == 3 ~ "10µ < 100µ",
+    size.category.noNA == 4 ~ "100µ < 1mm",
+    size.category.noNA == 5 ~ "1mm < 5mm",
+    size.category.noNA == 0 ~ "unavailable")) %>% # creates new column with nicer names.
+  mutate(size_f = factor(size_cat, levels = c("<1µ", "1µ < 10µ", "10µ < 100µ", "100µ < 1mm", "1mm < 5mm", "unavailable"))) %>% # order our different size levels.
+  # shape category data tidying.
+  mutate(shape.noNA = replace_na(shape, "unavailable")) %>% # replaces NAs to better relabel.
+  mutate(shape_f = factor(shape.noNA, levels = c("fiber", "fragment", "sphere", "unavailable"))) %>% # order our different shapes.
+  # polymer category data tidying.
+  mutate(polymer.noNA = replace_na(polymer, "unavailable")) %>% # replaces NA with 0 so we can better relabel it.
+  mutate(poly_f = factor(polymer.noNA, levels = c("BIO", "EVA", "PA", "PC", "PE", "PET", "PLA", "PMMA", "PP", "PS", "PUR", "PVC", "unavailable"))) # order our different polymers.
+
+#### Scott Setup ####
+
+# Create Shiny app. Anything in the sections below (user interface & server) should be the reactive/interactive parts of the shiny application.
 
 #### User Interface ####
 ui <- fluidPage(
@@ -59,10 +98,14 @@ ui <- fluidPage(
                     p("The figures below display data from the literature review of toxicological effects of microplastics on aquatic organisms. All data displayed - individual points and boxplots - are from studies in which there was a demonstrated significant toxicological effect of microplastics."),
                     br(), # line break
                     p("Each row of figures displays a different value along the y-axis - size, shape, and polymer, respectively. Each column of figures displays a different unit along the x-axis - mg/L and particles/mL, respectively. The data may be filtered by organism and/or endpoint using the selection widgets on the left-hand side of the window."),
+                    br(), # line break
+                    
                     sidebarPanel("Use the options below to filter the dataset.",
+                      br(), # line break
                       radioButtons(inputId = "organism_select", # multiple choice
                         label = "Choose an organism:",
-                        choices = unique(aoc$organism.group))),
+                        choices = unique(aoc$organism.group)),
+                    br()), # line break
                     mainPanel("Microplastics in Aquatic Environments Data Exploration of Toxicological EFfects",
                       p(" "),
                       plotOutput(outputId = "ssp_plot")), # patchwork plot
