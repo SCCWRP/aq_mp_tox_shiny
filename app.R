9cb9f80e446a9a1449fb88ac873ccc67d0ed7af3
emilydarin <emily.darin@student.csulb.edu>
2020-11-25 10:21:33 -0800

    Switched geom text labels

--- app.R
+++ app.R
@@ -65,9 +65,9 @@ get_plot_output_list <- function(input_n) {
         filter(plot_f==i) %>%
         
         # generate plot
-        ggplot(aes(fill=effect, y= logEndpoints, x=Type, Endpoints=Endpoints)) +
+        ggplot(aes(fill=effect, y= logEndpoints, x=Type, Freq=Freq)) +
         geom_bar(position="stack", stat="identity") +
-        geom_text(aes(label= paste0(Freq,"%")), position = position_stack(vjust = 0.5),colour="black") +
+        geom_text(aes(label= paste0(Endpoints)), position = position_stack(vjust = 0.5),colour="black") +
         scale_fill_manual(values = cal_palette(case_when(i=="Polymer"~"wetland", i=="Organism"~"sbchannel", i=="Size"~"seagrass",i=="Shape"~"gayophytum",i=="Endpoint Category"~"figmtn",i=="Life Stage"~"dudleya",i=="Exposure Route"~"halfdome",i=="In Vivo or In Vitro"~"kelp2")))+
         theme_classic() +
         ylab("Number of Endpoints Measured") +
@@ -81,7 +81,7 @@ get_plot_output_list <- function(input_n) {
           axis.text.y = element_blank(),
           axis.title.x = element_blank())
       
-      ggplotly(tooltip = 'Endpoints')%>%
+      ggplotly(tooltip = 'Freq')%>%
         config(displayModeBar = FALSE)
       
     })
