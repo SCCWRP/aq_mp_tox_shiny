91ebfbee4f4ce1488d1cc6057edfba74378e24a6
Coffin <Scott.Coffin@Waterboards.ca.gov>
2020-12-04 18:00:23 -0800

    cleaned up a bit

--- Simplifying via continuous probability distributions.R
+++ Simplifying via continuous probability distributions.R
@@ -7,6 +7,7 @@
 
 # Bimodal universal shape distribution for environmental microplastic from Kooi and Koelmans (2019)
 library(truncnorm)
+library(tidyverse)
 
 #average parameters from distribution
 f1 = 0.06
@@ -15,19 +16,18 @@ delta1 = 0.03
 delta2 = 0.19
 mu1 = 0.08
 mu2 = 0.44
-x_grid = rtruncnorm(n = 1000, a = 0.001, b = 1, mean = 0.4)
+x = runif(n=100)
 
 
 #define list from above
-param_list = list("f1" = f1, "f2" = f2, "delta1" = delta1, "delta2" = delta2, "mu1" = mu1, "mu2" = mu2, "x" = x_grid)
+param_list = list("f1" = f1, "f2" = f2, "delta1" = delta1, "delta2" = delta2, "mu1" = mu1, "mu2" = mu2, "x" = x)
 
 #permanently define euler's number
 e <- exp(1)
 lockBinding("e", globalenv())
 
 #starting conditions for x
-x = 1 #particle size in micrometers
-
+x = runif(n = 100) #particle size in micrometers
 
 #equation 4
 y = f1 * (1/sqrt(2 * pi * delta1 ^ 2)) * (e^-(x - mu1)^2 /2 *delta1 ^ 2)+ f2 * (1/sqrt(2 * pi * delta2 ^ 2)) * (e^-(x - mu2)^2 /2 *delta2 ^ 2)
@@ -35,20 +35,17 @@ y = f1 * (1/sqrt(2 * pi * delta1 ^ 2)) * (e^-(x - mu1)^2 /2 *delta1 ^ 2)+ f2 * (
 
 # Monte Carlo Simulation 
 library(MonteCarlo)
-
-
-library(truncnorm) #to make truncated normal distribution
 #define equation 4
 binom <- function(x, f1, f2, delta1, delta2, mu1, mu2){
   #generate sample
-  #x = abs(rnorm(1))
+  #x = runif(n=1000)
   #calculate CSF
   y <- f1 * (1/sqrt(2 * pi * delta1 ^ 2)) * (e^-(x - mu1)^2 /2 *delta1 ^ 2)+ f2 * (1/sqrt(2 * pi * delta2 ^ 2)) * (e^-(x - mu2)^2 /2 *delta2 ^ 2)
   #return result
-  return(list("y" = y, "x" = x))
+  return(list("y" = y))
 }
 
-MC_result <- MonteCarlo(func=binom, nrep=1000, param_list=param_list)
+MC_result <- MonteCarlo(func=binom, nrep=1, param_list=param_list)
 
 df<-MakeFrame(MC_result)
 head(df)
