acb750ab4e45d3a746365f8809bf5e2482ae67a4
Coffin <scott.coffin@waterboards.ca.gov>
2021-02-23 12:38:09 -0800

    slight changes

--- Simplifying via continuous probability distributions.R
+++ Simplifying via continuous probability distributions.R
@@ -54,4 +54,50 @@ library(ggplot2)
 tbl <- tibble(df)
 
 ggplot(tbl) + 
+  
+# From Merel Kooi
+rm(list=ls())
+
+## Parameters
+n = 1E5     #number of values to generate
+xmin = 1   #minimum size (um)
+xmax = 5000 #maximum size (um)
+alpha = 1.6 #exponent of power law distribution
+
+## Generate random data given parameters of the power law distribution
+## Based on PDF
+
+## First approach 
+a = -alpha     # It has to be negative
+y = runif(n)   # Number of samples
+Size1 = ((xmax^(a+1) - xmin^(a+1))*y + xmin^(a+1))^(1/(a+1))
+
+## Second approach
+## Function that will randomly generate 1 value from the distribution
+X.func <- function (X){
+  success <- FALSE
+  while (!success){
+    U = runif(1, 0, 1)
+    X = xmin*(1-U)^(1/(1-alpha))
+    success <- X < xmax }
+  return(X)
+}
+
+## df to store your generated results in
+Size2 <- data.frame(Size = numeric(0))
+
+## loop through function n times (can probably do this in a function too)
+for(i in 1:n){
+  X <- X.func()
+  Size2 <- rbind(Size2, X)
+}
+
+colnames(Size2) <- "Size2"
+
+## check results - visual comparison
+Size <- cbind(Size1, Size2)
+
+par(mfrow = c(1,2))
+hist(Size$Size1)
+hist(Size$Size2)
   geom_density(aes(y))
